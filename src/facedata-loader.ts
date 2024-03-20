7ab731d0fa7c94705f20bb37dd41d4a6eea25d68
Oliver Spindler <os@oliverspindler.com>
2024-03-20 20:41:04 +0100

    Disable broken Facedata validation

--- src/facedata-loader.ts
+++ src/facedata-loader.ts
@@ -1,14 +1,15 @@
 import merge from 'ts-deepmerge'
 
 import Facedata from './facedata'
-import validate from './facedata.d.validator'
+// import validate from './facedata.d.validator'
 
 export const loadFacedataFromJson = (...parts: unknown[]): Facedata => {
   const merged = merge(...parts)
-  try {
-    return validate(merged)
-  } catch (e) {
-    e.message = e.message.replace(/, /g, '\n')
-    throw e
-  }
+  return merged as Facedata
+  // try {
+  //   return validate(merged)
+  // } catch (e) {
+  //   e.message = e.message.replace(/, /g, '\n')
+  //   throw e
+  // }
 }
